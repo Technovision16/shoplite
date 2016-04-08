cef256940a96574620551bbde27297f50a02c984
Ankit Jain <ankitjain28may77@gmail.com>
2016-04-09 00:58:36 +0530

    First commit

---
+++ table.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXIST products (
+	id int primary key auto_increment unique not null,
+	product_code varchar(255) unique not null,
+	price int not null
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXIST register (
+	id int primary key auto_increment unique not null,
+	mob varchar(10) unique not null,
+	name varchar(255) not null,
+	customer id varchar(255) unique not null,
+	FOREIGN KEY (id) REFERENCES shop(id)
+) ENGINE=INNODB;
+
+
+
+CREATE TABLE IF NOT EXISTS shop (
+	id int primary key not null,
+	p_code varchar(255) not null,
+	price int not null
+) ENGINE=INNODB;
