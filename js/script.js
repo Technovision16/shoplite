847526f159872984b3a9fb34027e9b4cbdecd799
itaditya <adityaa803@gmail.com>
2016-04-09 05:14:26 +0530

    Additions

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ function send_count() {
 	var a=document.getElementById('counter').value=count;
 	console.log(a);
 }
+
+// Ajax   
+
+function showPrice(str) {
+     if (str.length == 0) {
+         document.getElementById("txtHint").innerHTML = "";
+         return;
+     } else {
+         var xmlhttp = new XMLHttpRequest();
+         xmlhttp.onreadystatechange = function() {
+             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                 document.getElementById("pr").innerHTML = xmlhttp.responseText;
+             }
+         }
+         xmlhttp.open("GET", "getprice.php?q="+str, true);
+         xmlhttp.send();
+     }
+}
